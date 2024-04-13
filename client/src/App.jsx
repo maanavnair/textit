434870ca543f6a4f3091aa6b615582d293fb6a65
Maanav Nair <maanavnair20042@gmail.com>
2024-04-13 18:57:24 +0530

    started sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <SignUp />
     </div>
   )
 }
