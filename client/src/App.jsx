d145da1af419c2c2911f957d455729bb1213465f
Maanav Nair <maanavnair20042@gmail.com>
2024-04-15 09:19:31 +0530

    working on home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
 
 const App = () => {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <SignUp />
+      <Home />
     </div>
   )
 }
