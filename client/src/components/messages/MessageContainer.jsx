510130d0f79d202be8276647ad9b5fe65f8126e1
Maanav Nair <maanavnair20042@gmail.com>
2024-04-16 19:43:48 +0530

    added get messages functionality

--- client/src/components/messages/MessageContainer.jsx
+++ client/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MessageInput from './MessageInput'
 import Messages from './Messages'
 import { TiMessages } from 'react-icons/ti'
+import useConversation from '../../zustand/useConversation';
 
 const MessageContainer = () => {
-    const noChatSelected = false;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+
+    useEffect(() => {
+
+        //cleanup function
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation])
+
   return (
     <div className='md:min-w-[450px] flex flex-col '>
-        {noChatSelected ? (
+        {!selectedConversation ? (
             <NoChatSelected />
         ) : (
         <>
             {/* Header */}
             <div className='bg-slate-500 px-4 py-2 mb-2'>
-                <span className='text-gray-900 font-bold'>John Doe</span>
+                <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
             </div>
             <Messages />
             <MessageInput />
---
+++ client/src/hooks/useGetConversations.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import toast from 'react-hot-toast';
+
+const useGetConversations = () => {
+    const [loading, setLoading] = useState(false); 
+    const [conversations, setConversations] = useState([]);
+
+    useEffect(() => {
+        const getConversations = async () => {
+            setLoading(true);
+            try{
+                const res = await fetch('http://localhost:3000/api/users', {
+                    credentials: 'include'
+                });
+                const data = await res.json();
+                if(data.error){
+                    throw new Error(data.error);
+                }
+                setConversations(data);
+            }
+            catch(err){
+                toast.error(err.message);
+            }
+            finally{
+                setLoading(false);  
+            }
+        }
+
+        getConversations();
+    }, [])
+
+    return { loading, conversations };
+}
+
+export default useGetConversations
