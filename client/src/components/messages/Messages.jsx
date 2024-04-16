bd041b2fabeec26edd8e84b50f5c0e2face40e0f
Maanav Nair <maanavnair20042@gmail.com>
2024-04-16 20:13:01 +0530

    working on getting msg

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
 
 const Messages = () => {
+
+  const {loading, messages} = useGetMessages();
+  console.log("Messages: ", messages)
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
         <Message />
